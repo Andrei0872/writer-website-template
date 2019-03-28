2dcdf85a2f25103c6c314167f1fb20440d051296
Andrei0872 <andreigtj01@gmail.com>
2019-03-28 12:23:49 +0200

    Add slider with book covers

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 (function () {
+    const $$ = document.querySelectorAll.bind(document);
 
     if (window.innerWidth < 812) {
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
     }
 
+    [...$$(".js-card")].forEach((card, index) => {
+        card.style.backgroundImage = `url(./img/carte${index + 1}.${index === 0 ? 'jpeg': 'jpg'})`;
+        card.style.backgroundSize = 'cover'
+        card.style.backgroundRepeat = 'no-repeat'
+    })
 })();
