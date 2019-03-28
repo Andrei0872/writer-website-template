945f59a97307e7f8bbc69ad8ed91b7c674fce3db
Andrei0872 <andreigtj01@gmail.com>
2019-03-28 13:29:09 +0200

    Add video section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 (function () {
     const $$ = document.querySelectorAll.bind(document);
+    const $ = document.querySelector.bind(document);
 
     if (window.innerWidth < 812) {
         let vh = window.innerHeight * 0.01;
@@ -10,5 +11,18 @@
         card.style.backgroundImage = `url(./img/carte${index + 1}.${index === 0 ? 'jpeg': 'jpg'})`;
         card.style.backgroundSize = 'cover'
         card.style.backgroundRepeat = 'no-repeat'
-    })
+    });
+
+    const prevVideo = $(".js-prev-video");
+    const nextVideo = $(".js-next-video");
+
+    let currentIndex = 0;
+
+    nextVideo.addEventListener('click', () => {
+
+    });
+
+    nextVideo.addEventListener('click', () => {
+
+    });
 })();
